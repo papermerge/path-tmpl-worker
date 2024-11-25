fc7a6ab32373f1992758c0a466b1a8cbff358714
Eugen Ciur <ciur.eugen@gmail.com>
2024-11-25 07:53:16 +0100

    add failing test

--- tests/test_api.py
+++ tests/test_api.py
@@ -22,3 +22,24 @@ def test_move_document(db_session, make_receipt):
     actual_breadcrumb = "/".join([a[1] for a in get_ancestors(db_session, doc.id)])
     actual_breadcrumb = "/" + actual_breadcrumb
     assert actual_breadcrumb == "/home/My Documents/Receipts/bon.pdf"
+
+
+def test_move_documents(db_session, make_receipt):
+    template_path = """
+        /home/My Documents/Receipts/
+    """
+    doc = make_receipt(title="bon.pdf", path_template=template_path)
+    api.move_documents(
+        db_session, document_type_id=doc.document_type_id, user_id=doc.user_id
+    )
+
+    refreshed_doc = db_session.execute(
+        select(orm.Document).where(orm.Document.id == doc.id)
+    ).scalar()
+
+    # title did not change
+    assert refreshed_doc.title == "bon.pdf"
+
+    actual_breadcrumb = "/".join([a[1] for a in get_ancestors(db_session, doc.id)])
+    actual_breadcrumb = "/" + actual_breadcrumb
+    assert actual_breadcrumb == "/home/My Documents/Receipts/bon.pdf"
