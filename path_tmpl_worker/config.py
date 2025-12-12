a2bfbeddb80daf01f84290ad3a1514c6339e2520
Eugen Ciur <ciur.eugen@gmail.com>
2025-12-12 08:44:55 +0100

    minor update

--- path_tmpl_worker/config.py
+++ path_tmpl_worker/config.py
@@ -7,7 +7,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     papermerge__redis__url: str | None = None
     papermerge__main__logging_cfg: Path | None = None
-    papermerge__database__url: str = "sqlite:////db/db.sqlite3"
+    papermerge__database__url: str
 
 
 @lru_cache()
