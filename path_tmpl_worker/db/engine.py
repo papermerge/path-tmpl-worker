e3f576b04be7bf70867dc9857b299f15c2b1047b
Eugen Ciur <ciur.eugen@gmail.com>
2025-12-13 06:12:28 +0100

    fix Ci

--- path_tmpl_worker/db/engine.py
+++ path_tmpl_worker/db/engine.py
@@ -9,10 +9,6 @@ SQLALCHEMY_DATABASE_URL = os.environ.get("PAPERMERGE__DATABASE__URL")
 connect_args = {}
 logger = logging.getLogger(__name__)
 
-SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
-    "postgresql://", "postgresql+syncpg://", 1
-)
-
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args=connect_args, poolclass=NullPool
 )
